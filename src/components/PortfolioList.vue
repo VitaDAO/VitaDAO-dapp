51a3f24c178b772e620de78a85ec4383af4c26c9
Johannes <johanneshage97@gmail.com>
2021-10-23 14:42:55 +0100

    first version of detail page with mock data

--- src/components/PortfolioList.vue
+++ src/components/PortfolioList.vue
@@ -5,104 +5,106 @@
       :key="ipnft.id"
       class="col-span-1 divide-gray-200 divide-y flex flex-col rounded-lg shadow"
     >
-      <div class="max-w-lg mx-auto grid gap-5 lg:max-w-none">
-        <div
-          class="
-            flex flex-col
-            rounded-lg
-            shadow-lg
-            overflow-hidden
-            bg-gradient-to-b
-            from-gray-800
-            to-gray-900
-          "
-        >
-          <div class="flex-shrink-0">
-            <img class="h-48 w-full object-cover" :src="ipnft.image" alt="" />
-          </div>
-          <div class="flex-1 p-6 flex flex-col justify-between">
-            <div class="flex-1">
-              <a href="#" class="block">
-                <p class="text-xl font-semibold text-white">{{ ipnft.name }}</p>
-                <p class="mt-3 text-base text-white line-clamp-3">
-                  {{ ipnft.description }}
-                </p>
-              </a>
+      <router-link :to="'/portfolio/' + ipnft.id">
+        <div class="max-w-lg mx-auto grid gap-5 lg:max-w-none">
+          <div
+            class="
+              flex flex-col
+              rounded-lg
+              shadow-lg
+              overflow-hidden
+              bg-gradient-to-b
+              from-gray-800
+              to-gray-900
+            "
+          >
+            <div class="flex-shrink-0">
+              <img class="h-48 w-full object-cover" :src="ipnft.image" alt="" />
             </div>
-            <div class="mt-6 flex items-center justify-between">
-              <p class="text-sm font-medium text-white">
-                <a href="#" class="hover:underline"> {{ ipnft.properties.research_lead }} </a>
-              </p>
-              <div class="flex truncate">
-                <a href="#">
-                  <span class="sr-only">{{ ipnft.properties.research_lead }}</span>
-                  <img
-                    class="h-10 w-10 rounded-full"
-                    src="https://pbs.twimg.com/profile_images/1170739893422755840/WrQL2WoC_400x400.jpg"
-                    alt=""
-                  />
+            <div class="flex-1 p-6 flex flex-col justify-between">
+              <div class="flex-1">
+                <a href="#" class="block">
+                  <p class="text-xl font-semibold text-white">{{ ipnft.name }}</p>
+                  <p class="mt-3 text-base text-white line-clamp-3">
+                    {{ ipnft.description }}
+                  </p>
+                </a>
+              </div>
+              <div class="mt-6 flex items-center justify-between">
+                <p class="text-sm font-medium text-white">
+                  <a href="#" class="hover:underline"> {{ ipnft.properties.research_lead }} </a>
+                </p>
+                <div class="flex truncate">
+                  <a href="#">
+                    <span class="sr-only">{{ ipnft.properties.research_lead }}</span>
+                    <img
+                      class="h-10 w-10 rounded-full"
+                      src="https://pbs.twimg.com/profile_images/1170739893422755840/WrQL2WoC_400x400.jpg"
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
+              <div class="mt-2">
+                <a href="#" class="inline-block">
+                  <span
+                    class="
+                      inline-flex
+                      items-center
+                      px-3
+                      py-0.5
+                      rounded-full
+                      text-sm
+                      font-medium
+                      bg-green-100
+                      text-green-800
+                    "
+                  >
+                    {{ ipnft.properties.industry }}
+                  </span>
+                </a>
+                <a href="#" class="inline-block px-1">
+                  <span
+                    class="
+                      inline-flex
+                      items-center
+                      px-3
+                      py-0.5
+                      rounded-full
+                      text-sm
+                      font-medium
+                      bg-blue-100
+                      text-blue-800
+                    "
+                  >
+                    {{ ipnft.properties.agreement_type }}
+                  </span>
                 </a>
               </div>
             </div>
-            <div class="mt-2">
-              <a href="#" class="inline-block">
-                <span
-                  class="
-                    inline-flex
-                    items-center
-                    px-3
-                    py-0.5
-                    rounded-full
-                    text-sm
-                    font-medium
-                    bg-green-100
-                    text-green-800
-                  "
-                >
-                  {{ ipnft.properties.industry }}
-                </span>
-              </a>
-              <a href="#" class="inline-block px-1">
-                <span
-                  class="
-                    inline-flex
-                    items-center
-                    px-3
-                    py-0.5
-                    rounded-full
-                    text-sm
-                    font-medium
-                    bg-blue-100
-                    text-blue-800
-                  "
-                >
-                  {{ ipnft.properties.agreement_type }}
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="mt-12 p-4 grid grid-cols-3 xl:mt-0 col-span-3 bg-gray-600">
-            <div>
-              <h3 class="text-xs font-semibold text-gray-300 tracking-wider uppercase">
-                Fractions
-              </h3>
-              <span class="text-base font-semibold text-white">17b</span>
-            </div>
-            <div>
-              <h3 class="text-xs font-semibold text-gray-300 tracking-wider uppercase">
-                Collectable/ DAO Owned
-              </h3>
-              <span class="text-base font-semibold text-white">20%/80%</span>
-            </div>
-            <div>
-              <h3 class="text-xs font-semibold text-gray-300 tracking-wider uppercase">
-                Implied Valuation
-              </h3>
-              <span class="text-base font-semibold text-white">$28,317,448</span>
+            <div class="mt-12 p-4 grid grid-cols-3 xl:mt-0 col-span-3 bg-gray-600">
+              <div>
+                <h3 class="text-xs font-semibold text-gray-300 tracking-wider uppercase">
+                  Fractions
+                </h3>
+                <span class="text-base font-semibold text-white">17b</span>
+              </div>
+              <div>
+                <h3 class="text-xs font-semibold text-gray-300 tracking-wider uppercase">
+                  Collectable/ DAO Owned
+                </h3>
+                <span class="text-base font-semibold text-white">20%/80%</span>
+              </div>
+              <div>
+                <h3 class="text-xs font-semibold text-gray-300 tracking-wider uppercase">
+                  Implied Valuation
+                </h3>
+                <span class="text-base font-semibold text-white">$28,317,448</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </router-link>
     </li>
   </ul>
 </template>
--- src/pages/IPNFTDetailPage.vue
+++ src/pages/IPNFTDetailPage.vue
@@ -1,6 +1,121 @@
 <template>
   <div class="w-full">
-    <h2 class="font-bold mb-2 text-white text-xl">{{ result.ipnft.name }}</h2>
+    <transition name="fade" mode="out-in">
+      <loading-indicator v-if="loading">Loading proposal…</loading-indicator>
+      <div v-else-if="error">Error: {{ error.message }}</div>
+      <div v-else-if="result && result.ipnft">
+        <nav class="flex max-w-full" aria-label="Breadcrumb">
+          <ol class="bg-black border border-white flex px-6 rounded-md space-x-4">
+            <li class="flex">
+              <div class="flex items-center">
+                <router-link to="/" class="hover:text-gray-300 text-white">
+                  <fa icon="home" />
+                  <span class="sr-only">Home</span>
+                </router-link>
+              </div>
+            </li>
+            <li class="flex">
+              <div class="flex items-center">
+                <svg
+                  class="flex-shrink-0 h-full text-white w-6"
+                  viewBox="0 0 24 44"
+                  preserveAspectRatio="none"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                </svg>
+                <router-link
+                  to="/portfolio"
+                  class="font-medium hover:text-gray-300 ml-4 text-white text-sm"
+                  >Portfolio</router-link
+                >
+              </div>
+            </li>
+            <li class="flex">
+              <div class="flex items-center">
+                <svg
+                  class="flex-shrink-0 h-full text-white w-6"
+                  viewBox="0 0 24 44"
+                  preserveAspectRatio="none"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                >
+                  <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                </svg>
+                <div class="font-medium ml-4 text-gray-400 text-sm">
+                  IP-NFT #{{ result.ipnft.id }}
+                </div>
+              </div>
+            </li>
+          </ol>
+        </nav>
+        <div class="gap-6 grid grid-cols-12 mt-4 lg:mt-6">
+          <div class="col-span-12 lg:col-span-7 xl:col-span-8 space-y-8">
+            <h1 class="font-bold leading-tight mt-4 text-2xl text-white">
+              {{ result.ipnft.name }}
+            </h1>
+            <span class="font-medium text-gray-300 text-sm"
+              >0xdf83baabf91236b5b329abc45616cc0fab2bf6aa (IP-NFT Address)
+            </span>
+            <span class="font-medium text-gray-400 text-sm">
+              <a
+                href="https://testnets.opensea.io/assets/0xdf83baabf91236b5b329abc45616cc0fab2bf6aa/47"
+              >
+                view on OpenSea
+              </a></span
+            >
+            <div class="mt-4 space-y-6">
+              <p class="text-sm text-white">
+                {{ result.ipnft.description }}
+              </p>
+            </div>
+            <div class="py-5">
+              <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-3">
+                <div class="sm:col-span-1">
+                  <dt class="text-sm font-medium text-gray-400">Area</dt>
+                  <dd class="mt-1 text-sm text-white">{{ result.ipnft.properties.industry }}</dd>
+                </div>
+                <div class="sm:col-span-1">
+                  <dt class="text-sm font-medium text-gray-400">Stage</dt>
+                  <dd class="mt-1 text-sm text-white">Preclinical</dd>
+                </div>
+                <div class="sm:col-span-1">
+                  <dt class="text-sm font-medium text-gray-400">Status</dt>
+                  <dd class="mt-1 text-sm text-white">Ongoing</dd>
+                </div>
+                <div class="sm:col-span-1">
+                  <dt class="text-sm font-medium text-gray-400">Investment</dt>
+                  <dd class="mt-1 text-sm text-white">$350.000</dd>
+                </div>
+                <div class="sm:col-span-1">
+                  <dt class="text-sm font-medium text-gray-400">Patent</dt>
+                  <dd class="mt-1 text-sm text-white">Not Filled Yet</dd>
+                </div>
+                <div class="sm:col-span-1">
+                  <dt class="text-sm font-medium text-gray-400">Unique Owners</dt>
+                  <dd class="mt-1 text-sm text-white">602</dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <div class="mt-2 lg:mt-0 col-span-12 lg:col-span-5 xl:col-span-4 space-y-8">
+            <div class="flex flex-wrap justify-center">
+              <div class="w-3/4 sm:w-1/2 px-4">
+                <img
+                  :src="result.ipnft.image"
+                  :alt="result.ipnft.name"
+                  class="shadow rounded max-w-full h-auto align-middle border-none"
+                />
+              </div>
+            </div>
+            <h2 class="text-white p-12 text-center bg-gray-500">placeholder @Alexander</h2>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -9,8 +124,11 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
+    const loading = false
+    const error = null
     const result = {
       ipnft: {
+        id: 1,
         name: 'The Longevity Molecule (IPNFT)',
         description:
           'The Scheibye-Knudsen lab has analyzed 1.5 billion prescriptions from 4.8 million individuals over 40 years in The Danish National Health Service Prescription Database and correlated this with the survival of individuals prescribed certain drugs. The Scheibye-Knudsen Lab has identified several FDA-approved medications that appear to have a strong effect on lifespan following analysis. This project focuses on testing and developing three of these small molecules as possible interventions in aging, with the first series of experiments testing these therapies in fruit flies and human cells. This is an exclusive license agreement for the IP resulting from these experiments.',
@@ -26,6 +144,8 @@ export default defineComponent({
     }
 
     return {
+      loading,
+      error,
       result,
     }
   },
