7545f88ff28b3eb2238272cbcf472dd7d389c59c
Schmackofant <schmackofant@protonmail.com>
2021-11-25 20:30:36 +0000

    added basic button story for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,35 @@
+import {app} from '@storybook/vue3';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faHome,
+  faSpinner,
+  faBars,
+  faWallet,
+  faFile,
+  faCoins,
+  faClock,
+  faFolderOpen,
+  faVoteYea,
+} from '@fortawesome/free-solid-svg-icons'
+import { faDiscord, faDiscourse } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+app.component('fa', FontAwesomeIcon);
+
 import "../src/index.css"
+
+library.add(
+  faHome,
+  faSpinner,
+  faBars,
+  faWallet,
+  faFile,
+  faCoins,
+  faClock,
+  faFolderOpen,
+  faDiscord,
+  faDiscourse,
+  faVoteYea,
+)
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
