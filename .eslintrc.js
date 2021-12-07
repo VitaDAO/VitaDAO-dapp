64cadb15bfe543c203f2738c857ab84179727236
Schmackofant <schmackofant@protonmail.com>
2021-12-07 17:07:57 +0100

    don't throw errors for prettier formatting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'prettier/prettier': ['warn', { endOfLine: 'auto' }],
     'no-console': 'off',
     'no-debugger': 'warn',
     'no-unused-vars': 'warn',
