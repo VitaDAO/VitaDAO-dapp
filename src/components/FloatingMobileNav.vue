6e0fc220afcaf82d225060c447e175ffe57e490b
Schmackofant <schmackofant@protonmail.com>
2021-12-07 19:00:35 +0100

    improved floating mobile nav

--- src/components/FloatingMobileNav.vue
+++ src/components/FloatingMobileNav.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="bottom-4 fixed flex items-center justify-between left-4 lg:hidden right-4 z-10">
-    <wallet-indicator-button />
-    <button
-      class="bg-gray-800 border border-transparent duration-200 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 font-bold hover:bg-gray-700 px-5 py-3 rounded-xl shadow-lg text-sm text-white transition-all"
-      @click="openNav"
+    <div class="overflow-hidden rounded-full shadow-lg lg:shadow-none">
+      <wallet-indicator-button />
+    </div>
+
+    <base-button @click="openNav" class="shadow-lg" type="primary" size="sm" icon="bars"
+      >Menu</base-button
     >
-      <fa icon="bars" size="lg" />
-    </button>
   </div>
 </template>
 
 <script>
 import WalletIndicatorButton from '@/components/WalletIndicatorButton'
+import BaseButton from '@/components/BaseButton.vue'
 
 export default {
   components: {
     WalletIndicatorButton,
+    BaseButton,
   },
   props: {},
   emits: ['open-nav'],
