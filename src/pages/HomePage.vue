58151e77356f2c7c9550240dfd3ebec864b2a133
Schmackofant <schmackofant@protonmail.com>
2021-12-14 17:18:36 +0100

    extracted the section in its own component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,47 +1,6 @@
 <template>
   <div class="w-full">
     <hr class="border-black mb-8" />
-    <h2 class="font-medium mb-4 text-black text-3xl">DAO Snapshot</h2>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-      <div
-        class="w-full h-48 bg-vita-purple rounded-2xl p-8 flex flex-col justify-between text-white"
-      >
-        <div>
-          <h2 class="text-3xl xl:text-4xl font-bold">
-            <span class="align-top text-lg xl:text-2xl font-normal">$</span>25,016,640
-          </h2>
-          <p class="">+14% ($3,709,104)<span class="pl-2 opacity-60">weekly</span></p>
-        </div>
-        <p>Treasury Value</p>
-      </div>
-
-      <div
-        class="w-full h-48 bg-vita-sunrise rounded-2xl p-8 flex flex-col justify-between text-black"
-      >
-        <h2 class="text-3xl xl:text-4xl font-bold">5,545,847</h2>
-        <p>$VITA Circulating Supply</p>
-      </div>
-
-      <div
-        class="w-full h-48 bg-vita-sunrise rounded-2xl p-8 flex flex-col justify-between text-black"
-      >
-        <h2 class="text-3xl xl:text-4xl font-bold">
-          <span class="align-top text-lg xl:text-2xl font-normal">$</span>25,000,000
-        </h2>
-        <p>$VITA Market Cap</p>
-      </div>
-
-      <div
-        class="w-full h-48 bg-vita-sunrise rounded-2xl p-8 flex flex-col justify-between text-black"
-      >
-        <h2 class="text-3xl xl:text-4xl font-bold">
-          <span class="align-top text-lg xl:text-2xl font-normal">$</span>350,000
-        </h2>
-        <p>Invested in research</p>
-      </div>
-    </div>
-
-    <hr class="border-black mb-8 mt-20" />
     <h2 class="font-medium mb-4 text-black text-3xl">Active proposals</h2>
     <transition name="fade" mode="out-in">
       <loading-indicator v-if="loading">Loading active proposals…</loading-indicator>
