8951dba1d524dfb0bf3c387ce14a3aa1b7e092e8
Schmackofant <schmackofant@protonmail.com>
2022-01-20 12:33:41 +0100

    update category icon so that governance icon functions as a fallback

--- src/components/SnapshotProposalBox.vue
+++ src/components/SnapshotProposalBox.vue
@@ -65,72 +65,6 @@
       </svg>
 
       <svg
-        v-else-if="category == 'Governance'"
-        width="74"
-        height="78"
-        viewBox="0 0 74 78"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M38.9998 62.3H14.6998C12.2998 62.3 10.2998 60.3 10.2998 57.9V13.9C10.2998 11.5 12.2998 9.5 14.6998 9.5H51.2998C53.6998 9.5 55.6998 11.5 55.6998 13.9V42"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M10.3 56.3H5.9C3.5 56.3 1.5 54.3 1.5 51.9V5.9C1.5 3.5 3.5 1.5 5.9 1.5H44.5C46.9 1.5 48.9 3.5 48.9 5.9V9.5"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M17.2002 18.1001H49.0002"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M58.3001 43.8999C58.8001 45.6999 61.0001 46.3999 62.5001 45.2999C64.6001 43.6999 67.6001 45.7999 66.7001 48.2999C66.1001 50.0999 67.4001 51.8999 69.3001 51.8999C72.0001 51.7999 73.1001 55.2999 70.9001 56.7999C69.3001 57.8999 69.3001 60.1999 70.9001 61.1999C73.1001 62.6999 72.0001 66.1999 69.3001 66.0999C67.4001 65.9999 66.1001 67.8999 66.7001 69.6999C67.6001 72.1999 64.7001 74.3999 62.5001 72.6999C61.0001 71.5999 58.8001 72.2999 58.3001 74.0999C57.5001 76.6999 53.9001 76.6999 53.2001 74.0999C52.7001 72.2999 50.5001 71.5999 49.0001 72.6999C46.9001 74.2999 43.9001 72.1999 44.8001 69.6999C45.4001 67.8999 44.1001 66.0999 42.2001 66.0999C39.5001 66.1999 38.4001 62.6999 40.6001 61.1999C42.2001 60.0999 42.2001 57.7999 40.6001 56.7999C38.4001 55.2999 39.5001 51.7999 42.2001 51.8999C44.1001 51.9999 45.4001 50.0999 44.8001 48.2999C43.9001 45.7999 46.8001 43.5999 49.0001 45.2999C50.5001 46.3999 52.7001 45.6999 53.2001 43.8999C53.9001 41.3999 57.5001 41.3999 58.3001 43.8999Z"
-          stroke="#6256EC"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M55.7002 68.5C60.9469 68.5 65.2002 64.2467 65.2002 59C65.2002 53.7533 60.9469 49.5 55.7002 49.5C50.4535 49.5 46.2002 53.7533 46.2002 59C46.2002 64.2467 50.4535 68.5 55.7002 68.5Z"
-          stroke="#6256EC"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M17.2002 26.1001H49.0002"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M17.2002 34H42.1002"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M17.2002 42H27.1002"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-      </svg>
-
-      <svg
         v-else-if="category == 'IP'"
         width="70"
         height="73"
@@ -162,73 +96,7 @@
       </svg>
 
       <svg
-        v-else-if="wasOnChainProposal"
-        width="74"
-        height="78"
-        viewBox="0 0 74 78"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M38.9998 62.3H14.6998C12.2998 62.3 10.2998 60.3 10.2998 57.9V13.9C10.2998 11.5 12.2998 9.5 14.6998 9.5H51.2998C53.6998 9.5 55.6998 11.5 55.6998 13.9V42"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M10.3 56.3H5.9C3.5 56.3 1.5 54.3 1.5 51.9V5.9C1.5 3.5 3.5 1.5 5.9 1.5H44.5C46.9 1.5 48.9 3.5 48.9 5.9V9.5"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M17.2002 18.1001H49.0002"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M58.3001 43.8999C58.8001 45.6999 61.0001 46.3999 62.5001 45.2999C64.6001 43.6999 67.6001 45.7999 66.7001 48.2999C66.1001 50.0999 67.4001 51.8999 69.3001 51.8999C72.0001 51.7999 73.1001 55.2999 70.9001 56.7999C69.3001 57.8999 69.3001 60.1999 70.9001 61.1999C73.1001 62.6999 72.0001 66.1999 69.3001 66.0999C67.4001 65.9999 66.1001 67.8999 66.7001 69.6999C67.6001 72.1999 64.7001 74.3999 62.5001 72.6999C61.0001 71.5999 58.8001 72.2999 58.3001 74.0999C57.5001 76.6999 53.9001 76.6999 53.2001 74.0999C52.7001 72.2999 50.5001 71.5999 49.0001 72.6999C46.9001 74.2999 43.9001 72.1999 44.8001 69.6999C45.4001 67.8999 44.1001 66.0999 42.2001 66.0999C39.5001 66.1999 38.4001 62.6999 40.6001 61.1999C42.2001 60.0999 42.2001 57.7999 40.6001 56.7999C38.4001 55.2999 39.5001 51.7999 42.2001 51.8999C44.1001 51.9999 45.4001 50.0999 44.8001 48.2999C43.9001 45.7999 46.8001 43.5999 49.0001 45.2999C50.5001 46.3999 52.7001 45.6999 53.2001 43.8999C53.9001 41.3999 57.5001 41.3999 58.3001 43.8999Z"
-          stroke="#6256EC"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M55.7002 68.5C60.9469 68.5 65.2002 64.2467 65.2002 59C65.2002 53.7533 60.9469 49.5 55.7002 49.5C50.4535 49.5 46.2002 53.7533 46.2002 59C46.2002 64.2467 50.4535 68.5 55.7002 68.5Z"
-          stroke="#6256EC"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M17.2002 26.1001H49.0002"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M17.2002 34H42.1002"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-        <path
-          d="M17.2002 42H27.1002"
-          stroke="black"
-          stroke-width="3"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-        />
-      </svg>
-
-      <svg
-        v-else
+        v-else-if="category == 'Project'"
         width="84"
         height="84"
         viewBox="0 0 84 84"
@@ -357,6 +225,73 @@
         <path
           d="M57.6001 19L64.3001 25.7"
           stroke="#6256EC"
+          stroke-width="3"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+        />
+      </svg>
+
+      <!-- show governance icon as fallback -->
+      <svg
+        v-else
+        width="74"
+        height="78"
+        viewBox="0 0 74 78"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M38.9998 62.3H14.6998C12.2998 62.3 10.2998 60.3 10.2998 57.9V13.9C10.2998 11.5 12.2998 9.5 14.6998 9.5H51.2998C53.6998 9.5 55.6998 11.5 55.6998 13.9V42"
+          stroke="black"
+          stroke-width="3"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+        />
+        <path
+          d="M10.3 56.3H5.9C3.5 56.3 1.5 54.3 1.5 51.9V5.9C1.5 3.5 3.5 1.5 5.9 1.5H44.5C46.9 1.5 48.9 3.5 48.9 5.9V9.5"
+          stroke="black"
+          stroke-width="3"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+        />
+        <path
+          d="M17.2002 18.1001H49.0002"
+          stroke="black"
+          stroke-width="3"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+        />
+        <path
+          d="M58.3001 43.8999C58.8001 45.6999 61.0001 46.3999 62.5001 45.2999C64.6001 43.6999 67.6001 45.7999 66.7001 48.2999C66.1001 50.0999 67.4001 51.8999 69.3001 51.8999C72.0001 51.7999 73.1001 55.2999 70.9001 56.7999C69.3001 57.8999 69.3001 60.1999 70.9001 61.1999C73.1001 62.6999 72.0001 66.1999 69.3001 66.0999C67.4001 65.9999 66.1001 67.8999 66.7001 69.6999C67.6001 72.1999 64.7001 74.3999 62.5001 72.6999C61.0001 71.5999 58.8001 72.2999 58.3001 74.0999C57.5001 76.6999 53.9001 76.6999 53.2001 74.0999C52.7001 72.2999 50.5001 71.5999 49.0001 72.6999C46.9001 74.2999 43.9001 72.1999 44.8001 69.6999C45.4001 67.8999 44.1001 66.0999 42.2001 66.0999C39.5001 66.1999 38.4001 62.6999 40.6001 61.1999C42.2001 60.0999 42.2001 57.7999 40.6001 56.7999C38.4001 55.2999 39.5001 51.7999 42.2001 51.8999C44.1001 51.9999 45.4001 50.0999 44.8001 48.2999C43.9001 45.7999 46.8001 43.5999 49.0001 45.2999C50.5001 46.3999 52.7001 45.6999 53.2001 43.8999C53.9001 41.3999 57.5001 41.3999 58.3001 43.8999Z"
+          stroke="#6256EC"
+          stroke-width="3"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+        />
+        <path
+          d="M55.7002 68.5C60.9469 68.5 65.2002 64.2467 65.2002 59C65.2002 53.7533 60.9469 49.5 55.7002 49.5C50.4535 49.5 46.2002 53.7533 46.2002 59C46.2002 64.2467 50.4535 68.5 55.7002 68.5Z"
+          stroke="#6256EC"
+          stroke-width="3"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+        />
+        <path
+          d="M17.2002 26.1001H49.0002"
+          stroke="black"
+          stroke-width="3"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+        />
+        <path
+          d="M17.2002 34H42.1002"
+          stroke="black"
+          stroke-width="3"
+          stroke-miterlimit="10"
+          stroke-linecap="round"
+        />
+        <path
+          d="M17.2002 42H27.1002"
+          stroke="black"
           stroke-width="3"
           stroke-miterlimit="10"
           stroke-linecap="round"
@@ -583,16 +518,14 @@ export default defineComponent({
     })
 
     const category = computed(function () {
-      if (props.proposal.title.startsWith('[Governance]')) {
-        return 'Governance'
+      if (props.proposal.title.startsWith('[Project]')) {
+        return 'Project'
       } else if (props.proposal.title.startsWith('[IP]')) {
         return 'IP'
       } else if (props.proposal.title.startsWith('[Funding]')) {
         return 'Funding'
-      } else if (wasOnChainProposal.value == true) {
-        return 'Governance'
       } else {
-        return 'Project'
+        return 'Governance'
       }
     })
 
