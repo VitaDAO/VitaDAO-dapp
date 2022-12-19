2d84ed6a5a2a4af19d46d2d0ddb89f768eec925b
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-12-19 17:12:59 +0200

    Improve usd value and delta on chart

--- src/components/TreasuryGraph.vue
+++ src/components/TreasuryGraph.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="rounded-2xl border border-gray-300 p-8 flex flex-col gap-3 min-h-[400px]">
+  <div class="rounded-2xl border border-gray-300 p-8 flex flex-col min-h-[400px]">
     <LoadingIndicator v-if="status === 'loading'">Loading chart…</LoadingIndicator>
     <template v-else-if="status === 'success'">
-      <span v-if="usdTotal" class="flex items-start text-xl gap-1">
+      <span class="flex items-start text-xl gap-1">
         $
-        <span class="font-medium text-[42px] leading-[1em]">
-          {{ usdTotal }}
-        </span>
+        <span v-if="usdTotal" class="font-medium text-[42px] leading-[1em]">{{ usdTotal }}</span>
       </span>
+      <span v-if="usdDelta" class="text-[20px] text-vita-purple">{{ usdDelta }}</span>
       <div v-if="Array.isArray(usdValues)" class="flex-1">
         <LineChart :chartData="usdValues" />
       </div>
-      <div class="flex gap-2">
+      <div class="flex gap-2 mt-3">
         <button
           v-for="option in timeRanges"
           :key="option"
@@ -45,11 +44,25 @@ const { data, status } = useQuery(['getTreasuryUsdTimeseries'], getTreasuryUsdTi
 const usdValues = computed(() => (Array.isArray(data.value) ? data.value.slice(-365) : undefined))
 
 const usdTotal = computed(() =>
-  Array.isArray(data.value)
-    ? data.value.at(-1).balance?.toLocaleString(undefined, {
+  Array.isArray(usdValues.value)
+    ? usdValues.value.at(-1).balance?.toLocaleString(undefined, {
         minimumFractionDigits: 0,
         maximumFractionDigits: 0,
       })
     : undefined,
 )
+
+const usdDelta = computed(() => {
+  if (Array.isArray(usdValues.value)) {
+    const start = usdValues.value.at(0).balance
+    const end = usdValues.value.at(-1).balance
+    const delta = end - start
+    const deltaPercent = (delta / end) * 100
+    const sign = delta > 0 ? '+' : ''
+    return `${sign}${deltaPercent.toPrecision(2)}% ($${Math.abs(delta).toLocaleString(undefined, {
+      maximumFractionDigits: 0,
+    })})`
+  }
+  return undefined
+})
 </script>
