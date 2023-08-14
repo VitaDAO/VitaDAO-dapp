780b80450dc096f78fa5e0d548e0fa4269fef6e1
Schmackofant <schmackofant@protonmail.com>
2023-08-14 15:51:19 +0200

    Update StakingBox.vue

--- src/components/StakingBox.vue
+++ src/components/StakingBox.vue
@@ -30,66 +30,48 @@
 
       <tab-panels class="mt-7">
         <tab-panel :class="['bg-white rounded-xl']">
-          <transition name="fade" mode="out-in">
-            <div v-if="!vitaIsApproved" key="approveFirst">
-              <notice-box v-if="vitaBalance == 0" class="mb-4"
-                ><p>
-                  You have to enable the VITA token in order to interact with the DAO contract.
-                </p></notice-box
-              >
-              <base-button
-                type="teal"
-                full-width
-                :disabled="approvingVita"
-                :loading="approvingVita"
-                @click="approveVitaToken"
-              >
-                Enable VITA Token
-              </base-button>
-            </div>
-            <div v-else key="unstaking">
-              <notice-box v-if="stakedVitaBalance == 0" class="mb-4"
-                ><p>You don't have any staked VITA at the moment.</p></notice-box
-              >
-              <div class="mb-6">
-                <label class="block font-semibold text-black text-sm">Amount</label>
-                <div class="flex mt-1 rounded-xl">
-                  <div class="w-full relative">
-                    <input
-                      v-model="unstakingAmount"
-                      placeholder="0.0"
-                      min="0"
-                      minlength="1"
-                      maxlength="79"
-                      spellcheck="false"
-                      autocomplete="off"
-                      autocorrect="off"
-                      inputmode="decimal"
-                      pattern="^[0-9]*[.]?[0-9]*$"
-                      type="text"
-                      class="block border-gray-300 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-vita-purple rounded-xl text-sm sm:text-base w-full py-2.5"
-                      @keypress="validateNumber"
-                    />
-                    <button
-                      @click="setUnstakingMax"
-                      class="bg-vita-sunrise px-3 py-1 absolute right-2 top-2 uppercase rounded-lg text-sm font-medium"
-                    >
-                      Max
-                    </button>
-                  </div>
+          <div key="unstaking">
+            <notice-box v-if="stakedVitaBalance == 0" class="mb-4"
+              ><p>You don't have any staked VITA at the moment.</p></notice-box
+            >
+            <div class="mb-6">
+              <label class="block font-semibold text-black text-sm">Amount</label>
+              <div class="flex mt-1 rounded-xl">
+                <div class="w-full relative">
+                  <input
+                    v-model="unstakingAmount"
+                    placeholder="0.0"
+                    min="0"
+                    minlength="1"
+                    maxlength="79"
+                    spellcheck="false"
+                    autocomplete="off"
+                    autocorrect="off"
+                    inputmode="decimal"
+                    pattern="^[0-9]*[.]?[0-9]*$"
+                    type="text"
+                    class="block border-gray-300 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-vita-purple rounded-xl text-sm sm:text-base w-full py-2.5"
+                    @keypress="validateNumber"
+                  />
+                  <button
+                    @click="setUnstakingMax"
+                    class="bg-vita-sunrise px-3 py-1 absolute right-2 top-2 uppercase rounded-lg text-sm font-medium"
+                  >
+                    Max
+                  </button>
                 </div>
               </div>
-              <base-button
-                type="secondary"
-                full-width
-                :loading="unstakingVita"
-                :disabled="unstakingAmount == 0 || unstakingVita"
-                @click="unstakeVita"
-              >
-                Unstake VITA
-              </base-button>
             </div>
-          </transition>
+            <base-button
+              type="secondary"
+              full-width
+              :loading="unstakingVita"
+              :disabled="unstakingAmount == 0 || unstakingVita"
+              @click="unstakeVita"
+            >
+              Unstake VITA
+            </base-button>
+          </div>
         </tab-panel>
         <tab-panel :class="['bg-white rounded-xl']">
           <transition name="fade" mode="out-in">
